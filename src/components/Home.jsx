1bc018032ad4843a99b8cfefcf1fb83d3a23335f
nerbertb <nerbertb@gmail.com>
2025-11-12 02:58:54 +0800

    feat: completed mobile UI

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,62 +1,171 @@
 function Home() {
-    return ( 
-        <div>
-            <p>
+  return (
+    <div className="cointainer max-w-[375px] my-[73px] mx-[35px] flex flex-col bg-[#F6F5F6]">
+      <div className="grid grid-cols-1 gap-8">
+        {/* DANIEL */}
+        <section className="flex flex-col gap-4 bg-[#733FC8] p-8 rounded-lg">
+          <div className="flex gap-[17px] items-center">
+            <div>
+              <img
+                src="./public/image-daniel.jpg"
+                alt="Daniel Clifford"
+                className="rounded-full size-7 border-2 border-[#A775F1]"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <h3 className="text-[13px]/[110%] text-white barlow-semi-condensed-medium">
                 Daniel Clifford
-  Verified Graduate
+              </h3>
+              <h4 className="text-[11px]/[110%] text-[#EDE4FF] barlow-semi-condensed-medium">
+                Verified Graduate
+              </h4>
+            </div>
+          </div>
+          <div className="text-xl/[120%] text-white barlow-semi-condensed-semibold max-w-[241px]">
+            I received a job offer mid-course, and the subjects I learned were
+            current, if not more so, in the company I joined. I honestly feel I
+            got every penny’s worth.
+          </div>
 
-  I received a job offer mid-course, and the subjects I learned were current, if not more so, 
-  in the company I joined. I honestly feel I got every penny’s worth.
+          <div className="text-[13px]/[140%] text-[#EDE4FF] barlow-semi-condensed-medium">
+            “ I was an EMT for many years before I joined the bootcamp. I’ve
+            been looking to make a transition and have heard some people who had
+            an amazing experience here. I signed up for the free intro course
+            and found it incredibly fun! I enrolled shortly thereafter. The next
+            12 weeks was the best - and most grueling - time of my life. Since
+            completing the course, I’ve successfully switched careers, working
+            as a Software Engineer at a VR startup. ”
+          </div>
+        </section>
+        {/* JONATHAN */}
+        <section className="flex flex-col gap-4 bg-[#48556A] p-8 rounded-lg">
+          <div className="flex gap-[17px] items-center">
+            <div>
+              <img
+                src="./public/image-jonathan.jpg"
+                alt="Jonathan Walters"
+                className="rounded-full size-7"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <h3 className="text-[13px]/[110%] text-white barlow-semi-condensed-medium">
+                Jonathan Walters
+              </h3>
+              <h4 className="text-[11px]/[110%] text-[#676D7E] barlow-semi-condensed-medium">
+                Verified Graduate
+              </h4>
+            </div>
+          </div>
+          <div className="text-xl/[120%] text-white barlow-semi-condensed-semibold max-w-[241px]">
+            The team was very supportive and kept me motivated
+          </div>
+          <div className="text-[13px]/[140%] text-[#E7EAEE] barlow-semi-condensed-medium">
+            “ I started as a total newbie with virtually no coding skills. I now
+            work as a mobile engineer for a big company. This was one of the
+            best investments I’ve made in myself. ”
+          </div>
+        </section>
+        {/* JEANETTE */}
+        <section className="flex flex-col gap-4 bg-white p-8 rounded-lg">
+          <div className="flex gap-[17px] items-center">
+            <div>
+              <img
+                src="./public/image-jeanette.jpg"
+                alt="Jeanette Harmon"
+                className="rounded-full size-7"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <h3 className="text-[13px]/[110%] text-[#48556A] barlow-semi-condensed-medium">
+                Jeanette Harmon
+              </h3>
+              <h4 className="text-[11px]/[110%] text-[#676D7E] barlow-semi-condensed-medium">
+                Verified Graduate
+              </h4>
+            </div>
+          </div>
+          <div className="text-xl/[120%] text-[#48556A] barlow-semi-condensed-semibold max-w-[241px]">
+            An overall wonderful and rewarding experience
+          </div>
+          <div className="text-[13px]/[140%] text-[#676D7E] barlow-semi-condensed-medium">
+            “ Thank you for the wonderful experience! I now have a job I really
+            enjoy, and make a good living while doing something I love. ”
+          </div>
+        </section>
+        {/* PATRICK */}
+        <section className="flex flex-col gap-4 bg-[#19202D] p-8 rounded-lg">
+          <div className="flex gap-[17px] items-center">
+            <div>
+              <img
+                src="./public/image-patrick.jpg"
+                alt="Patrick Abrams"
+                className="rounded-full size-7 border-2 border-[#A775F1]"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <h3 className="text-[13px]/[110%] text-white barlow-semi-condensed-medium">
+                Patrick Abrams
+              </h3>
+              <h4 className="text-[11px]/[110%] text-white barlow-semi-condensed-medium">
+                Verified Graduate
+              </h4>
+            </div>
+          </div>
+          <div className="text-xl/[120%] text-[#CFCFCF] barlow-semi-condensed-semibold max-w-[241px]">
+            Awesome teaching support from TAs who did the bootcamp themselves.
+            Getting guidance from them and learning from their experiences was
+            easy.
+          </div>
+          <div className="text-[13px]/[140%] text-[#E7EAEE] barlow-semi-condensed-medium">
+            “ The staff seem genuinely concerned about my progress which I find
+            really refreshing. The program gave me the confidence necessary to
+            be able to go out in the world and present myself as a capable
+            junior developer. The standard is above the rest. You will get the
+            personal attention you need from an incredible community of smart
+            and amazing people. ”
+          </div>
+        </section>
 
-  “ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a 
-  transition and have heard some people who had an amazing experience here. I signed up 
-  for the free intro course and found it incredibly fun! I enrolled shortly thereafter. 
-  The next 12 weeks was the best - and most grueling - time of my life. Since completing 
-  the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”
-
-  Jonathan Walters
-  Verified Graduate
-
-  The team was very supportive and kept me motivated
-
-  “ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer 
-  for a big company. This was one of the best investments I’ve made in myself. ”
-
-  Jeanette Harmon
-  Verified Graduate
-
-  An overall wonderful and rewarding experience
-
-  “ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living 
-  while doing something I love. ”
-
-  Patrick Abrams
-  Verified Graduate
-
-  Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and 
-  learning from their experiences was easy.
-
-  “ The staff seem genuinely concerned about my progress which I find really refreshing. The program 
-  gave me the confidence necessary to be able to go out in the world and present myself as a capable 
-  junior developer. The standard is above the rest. You will get the personal attention you need from 
-  an incredible community of smart and amazing people. ”
-
-  Kira Whittle
-  Verified Graduate
-
-  Such a life-changing experience. Highly recommended!
-
-  “ Before joining the bootcamp, I’ve never written a line of code. I needed some structure from 
-  professionals who can help me learn programming step by step. I was encouraged to enroll by a former 
-  student of theirs who can only say wonderful things about the program. The entire curriculum and staff 
-  did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team 
-  project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial 
-  could ever have. In fact, I’ve often referred to it during interviews as an example of my developent 
-  experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers. 
-  100% recommend! ”
-            </p>
-        </div>
-     );
+        {/* KIRA */}
+        <section className="flex flex-col gap-4 bg-white p-8 rounded-lg">
+          <div className="flex gap-[17px] items-center">
+            <div>
+              <img
+                src="./public/image-kira.jpg"
+                alt="Kira Whittle"
+                className="rounded-full size-7"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <h3 className="text-[13px]/[110%] text-[#48556A] barlow-semi-condensed-medium">
+                Kira Whittle
+              </h3>
+              <h4 className="text-[11px]/[110%] text-[#676D7E] barlow-semi-condensed-medium">
+                Verified Graduate
+              </h4>
+            </div>
+          </div>
+          <div className="text-xl/[120%] text-[#48556A] barlow-semi-condensed-semibold max-w-[241px]">
+            Such a life-changing experience. Highly recommended!
+          </div>
+          <div className="text-[13px]/[140%] text-[#676D7E] barlow-semi-condensed-medium">
+            “ Before joining the bootcamp, I’ve never written a line of code. I
+            needed some structure from professionals who can help me learn
+            programming step by step. I was encouraged to enroll by a former
+            student of theirs who can only say wonderful things about the
+            program. The entire curriculum and staff did not disappoint. They
+            were very hands-on and I never had to wait long for assistance. The
+            agile team project, in particular, was outstanding. It took my
+            learning to the next level in a way that no tutorial could ever
+            have. In fact, I’ve often referred to it during interviews as an
+            example of my developent experience. It certainly helped me land a
+            job as a full-stack developer after receiving multiple offers. 100%
+            recommend! ”
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
